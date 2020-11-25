cb5b0405bb23560b1774644fc7629cb4e43a06ad
Paul Lefay <paul.lefay49@gmail.com>
2020-11-25 10:26:10 +0100

    Remove useless routes

--- controllers/referentstableController.js
+++ controllers/referentstableController.js
@@ -11,31 +11,12 @@ const db = myqsl.createConnection({
 exports.getReferents = async (request, response) => {
     var isadmin = request.isadmin
     if (isadmin) {
-        db.query("SELECT DISTINCT u.iduser, u.firstname, u.lastname, c.clubname, c.shortname, c.idclub FROM users u LEFT JOIN referentof r ON u.iduser = r.iduser LEFT JOIN clubs c ON r.shortname = c.shortname ORDER BY u.lastname", async (error, results) => {
+        db.query("SELECT DISTINCT u.iduser, u.firstname, u.lastname, c.clubname, c.shortname, c.idclub FROM users u LEFT JOIN referentof r " +
+         "ON u.iduser = r.iduser LEFT JOIN clubs c ON r.shortname = c.shortname UNION SELECT DISTINCT u.iduser, u.firstname, u.lastname, c.clubname, c.shortname, c.idclub " +
+         " FROM clubs c LEFT JOIN referentof r ON r.shortname = c.shortname LEFT JOIN users u ON u.iduser = r.iduser", async (error, results) => {
             const array = Object.values(JSON.parse(JSON.stringify(results)))
             response.send(array)
         })
     }
 }
 
-exports.getReferentList = async (request, response) => {
-    var isadmin = request.isadmin
-    if (isadmin) {
-        db.query("SELECT u.iduser, u.firstname, u.lastname FROM users u", async (error, results) => {
-            const array = Object.values(JSON.parse(JSON.stringify(results)))
-            console.log(array)
-            response.send(array)
-        })  
-    }
-}
-
-exports.getShortnameList = async (request, response) => {
-    var isadmin = request.isadmin
-    if (isadmin) {
-        db.query("SELECT c.clubname, c.shortname, c.idclub FROM clubs c", async (error, results) => {
-            const array = Object.values(JSON.parse(JSON.stringify(results)))
-            console.log(array)
-            response.send(array)
-        })     
-    }
-}
